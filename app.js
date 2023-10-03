2b45895d054b60b13034835db80b8c5e81348118
ritacc12 <rita20920@gmail.com>
2023-10-03 18:18:11 +0800

    build the route

--- app.js
+++ app.js
@@ -2,18 +2,25 @@ const express = require('express')
 const { engine } =require('express-handlebars')
 const app = express()
 const port = 3000
+const generatePassword = require('./main')
+
 
 app.engine('.hbs', engine({extname: '.hbs'}))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 app.use(express.static('public'))
 
+//use body-parser
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
-  res.redirect('/generate-password')
+  res.render('index')
 })
 
-app.get('/generate-password',(req,res)=>{
-    res.render('index')
+app.post('/',(req,res)=>{
+    const data = req.body
+    const password = generatePassword(data)
+    res.render('index', {password: password , data: data})
 })
 
 app.listen(port, () => {
