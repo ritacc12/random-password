3a9b5db3fce337d213104141f9b983096de795ae
ritacc12 <rita20920@gmail.com>
2023-10-17 18:27:46 +0800

    adjust the meta width

--- main.js
+++ main.js
@@ -1,62 +1,51 @@
-function generatePassword(data){
-    const lowercase =  'abcdefghijklmnopqrstuvwxyz'
-    const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    const number =  '1234567890'
-    const symbol =  '~`!@#$%^&*()_-+={}?[]/<>;:'
-    const exclude = data.exclude
-    
-    const data = {
-        length: '12',
-        lowercase: 'on',
-        uppercase: 'on',
-        number: 'on',
-        exclude:'04'
+function generatePassword(data) {
+    const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const number = '1234567890';
+    const symbol = '~`!@#$%^&*()_-+={}?[]/<>;:';
+    const exclude = data.exclude;
+
+    let collection = '';
+    let password = '';
+
+    if (data.lowercase === "on") {
+        collection += lowercase;
     }
 
-    let collection = []
-    let password = ''
-
-    if(data.lowercase === "on"){
-        collection = collection.concat(lowercase.split(''))
+    if (data.uppercase === "on") {
+        collection += uppercase;
     }
 
-    if(data.uppercase === "on"){
-        collection = collection.concat(uppercase.split(''))
+    if (data.number === "on") {
+        collection += number;
     }
 
-    if(data.number === "on"){
-        collection = collection.concat(number.split(''))
+    if (data.symbol === "on") {
+        collection += symbol;
     }
 
-    if(data.symbol === "on"){
-        collection = collection.concat(symbol.split(''))
+    // Exclude Characters
+    if (exclude) {
+    collection = collection.filter(
+      char => !exclude.includes(char)
+    )
+  }
+
+    if (data.length < 4 || data.length > 16) {
+        return 'Please select a length between 4 - 16';
     }
 
-    //exclude Characters
-    if(exclude){
-        console.log(`exclude characters: ${data.exclude}`)
-        collection = collection.filter((word) => !exclude.includes(word))
-    }
-    console.log('collection', collection)
-
-    if(collection.length === 0){
-        return password = 'There is no valid conditions'
+    if(data.lowercase !=="on" && data.uppercase !=="on" && data.number !=="on" && data.symbol !=="on"){
+        return 'Please select at least one character type';   
     }
 
-    if(data.length < 4 || data.length > 16){
-        return password = 'Please select the number between 4~16'
+    for (let i = 0; i < data.length; i++) {
+        console.log(typeof data.length, data.length);
+        const randomIndex = Math.floor(Math.random() * collection.length);
+        password += collection[randomIndex];
     }
 
-    if(data.lowercase !=="on"|| data.uppercase !=="on" || data.number !== "on" || data.symbol !== "on"){
-        return password = 'Please select at least one'
-    }
-
-    for(i=0 ; i< data.length ; i++){
-        const collection = Math.floor(Math.random() * collection.length)
-        password += collection[collection]
-    }
-    return password
-    // console.log('This function will generate password')
+    return password;
 }
 
- export default generatePassword;
+module.exports = generatePassword;
